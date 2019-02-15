432eba3b1c06ec32648665176a246bb269d4b856
Chen Ji <chenji@ChendeMacBook-Pro.local>
2019-02-15 18:22:19 +0800

    change content for #2

--- LC#2-Add-2-Numbers.py
+++ LC#2-Add-2-Numbers.py
@@ -1,6 +1,35 @@
-A = 6640000000000000000000000000001
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-while A >= 1:
-    test = A % 10
-    A = int(A / 10)
-    print(A)
+class Solution:
+    def addTwoNumbers(self, l1: 'ListNode', l2: 'ListNode') -> 'ListNode':
+        result=l1value=l2value = 0
+
+        linkresult = ListNode(None)
+        prelinkresult = linkresult
+        header = linkresult
+
+        carry = 0
+        tmpvalue = 0
+        while l1!=None or l2!=None or carry:
+            linkresult = ListNode(None)
+            l1value = l1.val if l1!=None else 0
+            l2value = l2.val if l2!=None else 0
+            if l1value+l2value+carry>=10:
+                result = (l1value+l2value+carry)%10
+                carry = 1
+            else:
+                result = (l1value+l2value+carry)
+                carry = 0
+            linkresult.val = result
+            prelinkresult.next = linkresult
+            prelinkresult = linkresult
+            l1 = l1.next if l1!=None else l1
+            l2 = l2.next if l2!=None else l2
+
+
+        return header.next
+            
